a300681f5205f3950cc083eeb10b98d9e9b54483
divit-agrawal <73773075+divit-agrawal@users.noreply.github.com>
2021-10-20 18:04:32 +0530

    bug fixes

--- routes/getAllTweets.js
+++ routes/getAllTweets.js
@@ -8,27 +8,11 @@ const router = express.Router();
 router.get("/", (req, res) => {
   Tweet.find()
     .then((data) => {
-      const page = 1;
-      const limit = req.query.limit;
-      const startIndex = 0;
-      const endIndex = page * limit;
-      const result = data.slice(startIndex, endIndex);
       res.json(result);
     })
     .catch((err) => {
       res.json({ message: err });
     });
 });
-
-//specific company
-// router.get("/:postId", async (req, res) => {
-//   await Post.findById(req.params.postId)
-//     .then((data) => {
-//       res.json(data);
-//     })
-//     .catch((err) => {
-//       res.json({ message: err });
-//     });
-// });
 
 module.exports = router;
